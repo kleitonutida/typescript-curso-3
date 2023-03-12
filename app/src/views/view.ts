ace90bfe45359500a78a7817488c4bc13f59861c
Kleiton Utida <kleiton.utida@live.com>
2023-03-11 23:40:57 -0300

    Property decorator

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,6 +1,3 @@
-import { inspect } from "../decorators/inspect.js";
-import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
-
 export abstract class View<T> {
   protected elemento: HTMLElement;
 
@@ -13,8 +10,6 @@ export abstract class View<T> {
     }
   }
 
-  @logarTempoDeExecucao(true)
-  @inspect
   public update(model: T): void {
     const template = this.template(model);
     this.elemento.innerHTML = template;
