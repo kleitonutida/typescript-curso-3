424947f2e88d4a29a954d6fc2914868551cee4a6
Kleiton Utida <kleiton.utida@live.com>
2023-03-13 23:46:08 -0300

    Polymorphism

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,17 +1,12 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao extends Imprimivel {
   constructor(
     private _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
-  ) {}
-
-  get volume(): number {
-    return this.quantidade * this.valor;
-  }
-
-  get data(): Date {
-    const data = new Date(this._data.getTime());
-    return data;
+  ) {
+    super();
   }
 
   public static criaDe(
@@ -24,5 +19,18 @@ export class Negociacao {
     const quantidade = parseInt(quantidadeString);
     const valor = parseFloat(valorString);
     return new Negociacao(date, quantidade, valor);
+  }
+
+  get volume(): number {
+    return this.quantidade * this.valor;
+  }
+
+  get data(): Date {
+    const data = new Date(this._data.getTime());
+    return data;
+  }
+
+  public paraTexto(): string {
+    return `Data: ${this.data}, Quantidade: ${this.quantidade}, Valor: ${this.valor}`;
   }
 }
